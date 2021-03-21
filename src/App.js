645311789d9e8dcc068d5e26f2e655c32ea75c20
Ronaldi Tjaidianto <ronalditjaidianto01@gmail.com>
2021-03-21 14:14:31 +0800

    [CHORES] Initial routing set up

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello World!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <div className="content">
+          <Switch>
+            <Route path="/masuk-investor">
+              
+            </Route>
+            <Route path="/daftar-investor">
+              
+            </Route>
+            <Route path="/masuk-mitra">
+              
+            </Route>
+            <Route path="/daftar-mitra">
+              
+            </Route>
+            <Route path="/tentang">
+              
+            </Route>
+            <Route path="/bantuan">
+              
+            </Route>
+            <Route exact path="/">
+              <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                  Hello World!
+                </p>
+                <a
+                  className="App-link"
+                  href="https://reactjs.org"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Learn React
+                </a>
+              </header>
+            </Route>
+          </Switch>
+        </div>
+      </div>
+    </Router>
   );
 }
 
