2c835574a600aa58eb0a1bf9d235555e02d4a05c
Aji Inisti <aji.inisti@ui.ac.id>
2021-03-27 09:06:01 +0700

    [RED] Add test for trying Link to Regis Investor

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,7 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Learn React/i);
+  const regis = screen.getByText(/Registrasi disini/i);
   expect(linkElement).toBeInTheDocument();
 });
