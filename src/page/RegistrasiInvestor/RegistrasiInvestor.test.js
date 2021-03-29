b2c9fb8f5f6273bf060faf5381a3ea805913506e
Aji Inisti <aji.inisti@ui.ac.id>
2021-03-29 22:19:58 +0700

    [RED] Test functionality Regis Investor

--- src/page/RegistrasiInvestor/RegistrasiInvestor.test.js
+++ src/page/RegistrasiInvestor/RegistrasiInvestor.test.js
@@ -2,16 +2,30 @@ import RegistrasiInvestor from './RegistrasiInvestor';
 import { render,screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import '@testing-library/jest-dom';
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter,Route } from 'react-router-dom'
+import { Provider } from 'react-redux' 
+import axios from 'axios'
+import configureStore from 'redux-mock-store'
+import thunk from 'redux-thunk'
 
+jest.mock('axios')
+
+const middlewares = [thunk]
+const mockStore = configureStore(middlewares)
 
 describe('<RegistrasiInvestor />', () => {
+
     it('renders the right contents', () => {
+        const mockSignUp = jest.fn()
+        const mockAuthenticate = jest.fn()
+        const initialState = { auth: {isAuthenticated: false}}
+        const store = mockStore(initialState)
         render(
-            <BrowserRouter>
-            <RegistrasiInvestor/>
-            </BrowserRouter>
-            );
+            <Provider store={store}>
+                <BrowserRouter>
+                    <RegistrasiInvestor signup={mockSignUp} isAuthenticated={mockAuthenticate}/>
+                </BrowserRouter>
+            </Provider>);
 
         expect(screen.getByLabelText(/Nama Depan/)).toBeInTheDocument();
         expect(screen.getByLabelText(/Nama Belakang/)).toBeInTheDocument();
@@ -21,46 +35,191 @@ describe('<RegistrasiInvestor />', () => {
     });
 
     it('renders correctly', () => {
+        const mockSignUp = jest.fn()
+        const mockAuthenticate = jest.fn()
+        const initialState = { auth: {isAuthenticated: false}}
+        const store = mockStore(initialState)
         const { getByText } = render(
-            <BrowserRouter>
-            <RegistrasiInvestor/>
-            </BrowserRouter>
-            );
+            <Provider store={store}>
+                <BrowserRouter>
+                    <RegistrasiInvestor signup={mockSignUp} isAuthenticated={mockAuthenticate}/>
+                </BrowserRouter>
+            </Provider>);
         expect(getByText(/Buat Akun Baru Investor/)).toBeInTheDocument();
     });
 
+    it('should redirect if authenticated', () => {
+        let loc;
+        const mockSignUp = jest.fn()
+        const initialState = { auth: {isAuthenticated: true}}
+        const store = mockStore(initialState)
+        render(
+            <Provider store={store}>
+                <BrowserRouter>
+                <RegistrasiInvestor signup={mockSignUp} isAuthenticated={true}/>
+                    <Route
+                    path="*"
+                    render={({location}) => {
+                        loc = location;
+                        return null;
+                    }}
+                    />
+                </BrowserRouter>
+            </Provider>
+        );
+        expect(loc.pathname).toBe('/');
+      })
+    
 
     test('button should have the corrrect link', () => {
+        const mockSignUp = jest.fn()
+        const mockAuthenticate = jest.fn()
+        const initialState = { auth: {isAuthenticated: false}}
+        const store = mockStore(initialState)
         const { getByText } = render(
-        <BrowserRouter>
-        <RegistrasiInvestor />
-        </BrowserRouter>);
-        expect(getByText('Masuk disini')).toHaveAttribute("href", '/login');
+        <Provider store={store}>
+            <BrowserRouter>
+                <RegistrasiInvestor signup={mockSignUp} isAuthenticated={mockAuthenticate}/>
+            </BrowserRouter>
+        </Provider>);
+        expect(getByText('Masuk disini')).toHaveAttribute("href", '/masuk');
     });
 
     test('useState Function testing', () => {
-        //Rendering the component and its tree
+        const mockSignUp = jest.fn()
+        const mockAuthenticate = jest.fn()
+        const initialState = { auth: {isAuthenticated: false}}
+        const store = mockStore(initialState)
         const { getByLabelText } = render(
-            <BrowserRouter>
-            <RegistrasiInvestor />
-            </BrowserRouter>);
-        //Extracting the child, username_input component with his accessibilityLabel
+            <Provider store={store}>
+                <BrowserRouter>
+                    <RegistrasiInvestor signup={mockSignUp} isAuthenticated={mockAuthenticate}/>
+                </BrowserRouter>
+            </Provider>);
+            
         const nama_depan = getByLabelText('Nama Depan');
         const nama_belakang = getByLabelText('Nama Belakang');
         const email = getByLabelText('Email');
         const password = getByLabelText('Kata Sandi');
         const re_password = getByLabelText('Konfirmasi kata sandi');
-        //Fire a native changeText event with a specific value
+        
         userEvent.type(nama_depan, "doe");
         userEvent.type(nama_belakang, 'joe');
         userEvent.type(email, 'doe@joe.com');
         userEvent.type(password, 'MyAwesome123');
         userEvent.type(re_password, 'MyAwesome123');
-        //Checking the rendered value 
+        
         expect(screen.getByLabelText('Nama Depan')).toHaveValue('doe');
         expect(screen.getByLabelText('Nama Belakang')).toHaveValue('joe');
         expect(screen.getByLabelText('Email')).toHaveValue('doe@joe.com');
         expect(screen.getByLabelText('Kata Sandi')).toHaveValue('MyAwesome123');
         expect(screen.getByLabelText('Konfirmasi kata sandi')).toHaveValue('MyAwesome123');
-      });
+    });
+
+    test('submit through form', () => {
+        const mockSignUp = jest.fn()
+        const mockAuthenticate = jest.fn()
+        const initialState = { auth: {isAuthenticated: false}}
+        const store = mockStore(initialState)
+        const { getByLabelText } = render(
+            <Provider store={store}>
+                <BrowserRouter>
+                    <RegistrasiInvestor signup={mockSignUp} isAuthenticated={mockAuthenticate}/>
+                </BrowserRouter>
+            </Provider>);
+            
+        const signupData = {
+             data: {
+                first_name: "Udma", 
+                last_name: "Wijaya", 
+                role: "Investor", 
+                email: "ajiinisti12@gmail.com", 
+                id: 1
+            } 
+        }
+
+        const nama_depan = getByLabelText('Nama Depan');
+        const nama_belakang = getByLabelText('Nama Belakang');
+        const email = getByLabelText('Email');
+        const password = getByLabelText('Kata Sandi');
+        const re_password = getByLabelText('Konfirmasi kata sandi');
+        
+        axios.post.mockImplementationOnce(() => Promise.resolve(signupData));
+        
+        userEvent.type(nama_depan, "doe");
+        userEvent.type(nama_belakang, 'joe');
+        userEvent.type(email, 'doe@joe.com');
+        userEvent.type(password, 'MyAwesome123');
+        userEvent.type(re_password, 'MyAwesome123');
+        userEvent.click(screen.getByText('Buat Akun'));
+
+        expect(axios.post).toHaveBeenCalledTimes(1);
+    });
+
+    test('Fail submit through form', () => {
+        const mockSignUp = jest.fn()
+        const mockAuthenticate = jest.fn()
+        const initialState = { auth: {isAuthenticated: false}}
+        const store = mockStore(initialState)
+        const { getByLabelText } = render(
+            <Provider store={store}>
+                <BrowserRouter>
+                    <RegistrasiInvestor signup={mockSignUp} isAuthenticated={mockAuthenticate}/>
+                </BrowserRouter>
+            </Provider>);
+            
+        const signupData = {
+             data: {
+                first_name: "Udma", 
+                last_name: "Wijaya", 
+                role: "Investor", 
+                email: "ajiinisti12@gmail.com", 
+                id: 1
+            } 
+        }
+
+        const nama_depan = getByLabelText('Nama Depan');
+        const nama_belakang = getByLabelText('Nama Belakang');
+        const email = getByLabelText('Email');
+        const password = getByLabelText('Kata Sandi');
+        const re_password = getByLabelText('Konfirmasi kata sandi');
+        
+        axios.post.mockImplementationOnce(() => Promise.reject(signupData));
+        
+        userEvent.type(nama_depan, "doe");
+        userEvent.type(nama_belakang, 'joe');
+        userEvent.type(email, 'doe@joe.com');
+        userEvent.type(password, 'MyAwesome123');
+        userEvent.type(re_password, 'MyAwesome123');
+        userEvent.click(screen.getByText('Buat Akun'));
+
+        expect(axios.post).toHaveBeenCalledTimes(1);
+    });
+    
+    test('Fail SignUp Password Not Same', () => {
+        const mockSignUp = jest.fn()
+        const mockAuthenticate = jest.fn()    
+        const initialState = { auth: {isAuthenticated: false}}
+        const store = mockStore(initialState)
+        const { getByLabelText } = render(
+            <Provider store={store}>
+                <BrowserRouter>
+                    <RegistrasiInvestor signup={mockSignUp} isAuthenticated={mockAuthenticate}/>
+                </BrowserRouter>
+            </Provider>);
+            
+        const nama_depan = getByLabelText('Nama Depan');
+        const nama_belakang = getByLabelText('Nama Belakang');
+        const email = getByLabelText('Email');
+        const password = getByLabelText('Kata Sandi');
+        const re_password = getByLabelText('Konfirmasi kata sandi');
+        
+        userEvent.type(nama_depan, "doe");
+        userEvent.type(nama_belakang, 'joe');
+        userEvent.type(email, 'doe@joe.com');
+        userEvent.type(password, 'MyAwesome123');
+        userEvent.type(re_password, 'AwesomeMy123');
+        userEvent.click(screen.getByText('Buat Akun'));
+    });
+
 });
