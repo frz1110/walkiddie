a6d3f9643428fb3672bfc9986eaa4d417f4adf3a
adhytianara <muhamad.adhytia@ui.ac.id>
2021-04-27 11:38:35 +0700

    [GREEN] Implement daftar toko

--- src/page/DaftarToko/DaftarToko.js
+++ src/page/DaftarToko/DaftarToko.js
@@ -8,9 +8,12 @@ import 'react-dates/lib/css/_datepicker.css';
 import AlurPendaftaran from './daftar-toko.svg';
 import React, { useState } from 'react';
 import Select from 'react-select';
-import { Link } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
+import axios from 'axios';
 
-const DaftarToko = () => {
+const DaftarToko = ({ isAuthenticated }) => {
+
     const [formData, setFormData] = useState({
         namaToko: '',
         namaCabang: '',
@@ -18,8 +21,14 @@ const DaftarToko = () => {
         nomorTelepon: '',
         deskripsiToko: '',
         lokasiToko: '',
-        totalBiaya: 1000000,
-        estimasiKeuangan: '',
+        latitude: -6.364520803098946,
+        longitude: 106.82922538589406,
+        mediaTokoList: [],
+        paketMainan: '',
+        totalBiaya: '',
+        periodePengadaanMulai: '',
+        periodePengadaanAkhir: '',
+        estimasiKeuangan: ''
     });
 
     const {
@@ -29,43 +38,46 @@ const DaftarToko = () => {
         nomorTelepon,
         deskripsiToko,
         lokasiToko,
-        mediaToko,
+        latitude,
+        longitude,
+        mediaTokoList,
         paketMainan,
         totalBiaya,
-        periodePengadaan,
+        periodePengadaanMulai,
+        periodePengadaanAkhir,
         estimasiKeuangan
     } = formData;
 
     const [startDate, setStartDate] = useState(null);
     const [endDate, setEndDate] = useState(null);
     const [focusedInput, setFocusedInput] = useState(null);
+
+    let file = '';
+
+    const onChange = e => setFormData({ ...formData, [e.target.name]: e.target.value });
+
     const handleDatesChange = ({ startDate, endDate }) => {
-        // if (startDate !== null && endDate !== null) {
-            setStartDate(startDate);
-            console.log(startDate);
-            setEndDate(endDate);
-            console.log(endDate)
-        // }
+        setStartDate(startDate);
+        console.log("startDate");
+        console.log(startDate);
+        setEndDate(endDate);
+        console.log("endDate")
+        console.log(endDate)
         if (startDate !== null && endDate !== null) {
+            setFormData({
+                ...formData, 
+                periodePengadaanMulai: startDate.format("YYYY-MM-DD"), 
+                periodePengadaanAkhir: endDate.format("YYYY-MM-DD")
+            })
+            console.log("startDate.format(YYYY-MM-DD)");
             console.log(startDate.format("YYYY-MM-DD"));
             console.log(endDate.format("YYYY-MM-DD"))
         }
     };
 
-    const onChange = e => setFormData({ ...formData, [e.target.name]: e.target.value });
-    
-    let file = '';
-
-    const media = {
-        files : []
-    }
-
     const handleChangeFile = (event) => {
-        media.files = event.target.files
-        console.log(event.target.files)
-        console.log(media.files)
+        setFormData({ ...formData, mediaTokoList: event.target.files });
     }
-
 
     const dataPilihanMainan = [
         {
@@ -87,13 +99,88 @@ const DaftarToko = () => {
       const handleChange = e => {
         setSelectedValue(e.value);
         if (e.value === "PaketA"){
-            setFormData({totalBiaya:1000000})
+            setFormData({...formData, totalBiaya:1000000, paketMainan: e.value})
         }else if (e.value === "PaketB"){
-            setFormData({totalBiaya:900000})
+            setFormData({...formData, totalBiaya:900000, paketMainan: e.value})
         }else {
-            setFormData({totalBiaya:800000})
+            setFormData({...formData, totalBiaya:800000, paketMainan: e.value})
         }
       }
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        postDaftarToko();
+    }
+
+    const postDaftarToko = () => {
+        if (localStorage.getItem('access')) {
+            console.log("inside if");
+            var formDataToSend = new FormData();
+
+            const config = {
+                headers: {
+                    'Content-Type': 'multipart/form-data',
+                    'Authorization': `JWT ${localStorage.getItem('access')}`,
+                }
+            };
+
+            console.log("paketMainan " + paketMainan);
+            console.log("totalBiaya " + totalBiaya);
+            console.log("startDate " + startDate);
+            console.log("endDate " + endDate);
+
+            formDataToSend.append('namaToko', namaToko);
+            formDataToSend.append('namaCabang', namaCabang);
+            formDataToSend.append('tipeUsaha', tipeUsaha);
+            formDataToSend.append('nomorTelepon', nomorTelepon);
+            formDataToSend.append('deskripsiToko', deskripsiToko);
+            formDataToSend.append('lokasiToko', lokasiToko);
+            formDataToSend.append('longitude', longitude);
+            formDataToSend.append('latitude', latitude);
+            formDataToSend.append('paketMainan', paketMainan);
+            for (let file of mediaTokoList) {
+                console.log(file);
+                formDataToSend.append('mediaToko', file);
+            }
+            formDataToSend.append('totalBiaya', totalBiaya);
+            formDataToSend.append('periodePengadaanMulai', periodePengadaanMulai);
+            formDataToSend.append('periodePengadaanAkhir', periodePengadaanAkhir);
+            formDataToSend.append('estimasiKeuangan', estimasiKeuangan);
+
+
+            try {
+                axios.post(`${process.env.REACT_APP_BACKEND_API_URL}/api/toko/`, formDataToSend, config)
+                    .then((response) => {
+                        console.log(response);
+                    }, (error) => {
+                        if (error.response) {
+
+                            console.log("error.response")
+                            console.log(error.response)
+
+                        } else if (error.request) {
+
+                            console.log("error.request")
+                            console.log(error.request)
+
+                        } else if (error.message) {
+
+                            console.log("error.message")
+                            console.log(error.message)
+
+                        }
+                        console.log(error);
+                    });
+                console.log('Success post');
+            } catch (err) {
+                console.log('Error post');
+            }
+        } else {
+            console.log('missing token');
+        }
+    }
+
+    if (!isAuthenticated) return <Redirect to="/masuk" />
 
     return (
         <div>
@@ -128,7 +215,7 @@ const DaftarToko = () => {
                         <br></br>
                         <h3 className="midtext" ><span>Informasi Toko</span></h3>
                         <br></br>
-                        <form className="centered">
+                        <form className="centered" onSubmit={handleSubmit}>
                             <div className="justify-content-center">
                                 <div className="form-group row">
                                     <label for='namaToko' class="col-sm-3 col-form-label"> <span class="required"> * </span> Nama Toko </label>
@@ -247,7 +334,7 @@ const DaftarToko = () => {
                                         <Select 
                                         class="form-control"
                                         placeholder="Pilih paket Mainan"
-                                        value={dataPilihanMainan.find(obj => obj.value === selectedValue)}
+                                        // value={dataPilihanMainan.find(obj => obj.value === selectedValue)}
                                         options={dataPilihanMainan}
                                         onChange={handleChange}
                                         />
@@ -304,4 +391,8 @@ const DaftarToko = () => {
     );
 }
 
-export default DaftarToko;
+const mapStateToProps = (state) => ({
+    isAuthenticated: state.auth.isAuthenticated
+})
+
+export default connect(mapStateToProps)(DaftarToko);
