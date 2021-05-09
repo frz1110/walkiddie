cdc53bc16d21a789db02f5143a2875b1673fc38d
Muhammad Ihsan Azizi <muhammad.ihsan83@ui.ac.id>
2021-05-10 00:08:08 +0700

    Solve minor bug PBI 5 mengisi profile

--- src/page/Profile/Profile.js
+++ src/page/Profile/Profile.js
@@ -54,7 +54,7 @@ const Profile = ({ load_user, isAuthenticated }) => {
                 setIsFilled(true);
             }
             catch (err) {
-
+                setIsFilled(false);
             }
         }
 
