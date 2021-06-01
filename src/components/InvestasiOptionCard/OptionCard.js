87eb210626562248aabafe1a1ca4408ceb573b3b
Ronaldi Tjaidianto <ronaldi.tjaidianto@ui.ac.id>
2021-06-01 21:49:36 +0700

    [REFACTOR] Resolve lint issue
    Remove unused variable

--- src/components/InvestasiOptionCard/OptionCard.js
+++ src/components/InvestasiOptionCard/OptionCard.js
@@ -1,8 +1,8 @@
-const OptionCard = ({ratio, amount}) => {
+const OptionCard = ({ratio, amount, isDisabled}) => {
     return ( 
         <div className="px-2 py-1" data-testid="mi-opt-card">
-            <label className="mi-card col p-3">
-                <input type="radio" className="mi-option-radio" name="mi-amount" value={amount} data-testid="mi-radio-btn"/>
+            <label className={isDisabled ? "mi-card col p-3 mi-card-disabled" : "mi-card col p-3"}>
+                <input type="radio" className="mi-option-radio" name="mi-amount" value={amount} data-testid="mi-radio-btn" disabled={isDisabled}/>
                 <span className="mi-option-subtitle mi-option-info-margin mt-5">{ratio}% dari total pendanaan</span>
                 <hr className="mi-option-divider mt-2 mb-4"/>
                 <span className="mi-option-nominal mi-option-info-margin">Rp {amount.toLocaleString()}</span>
