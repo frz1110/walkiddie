63ab8675eb676eaf46249c96cd828b2fdf717235
Fathinah Izzati <fathinahai@gmail.com>
2021-11-05 02:46:47 +0700

    test: add test for custom option cards

--- src/components/InvestasiOptionCard/CustomOptionCard.test.js
+++ src/components/InvestasiOptionCard/CustomOptionCard.test.js
@@ -1,18 +1,35 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import CustomOptionCard from './CustomOptionCard';
 import userEvent from '@testing-library/user-event';
 
+
 describe('<CustomOptionCard />', () => {
     it('should render correctly', () => {
-        const { getByText } = render(<CustomOptionCard />)
-        expect(getByText(/jumlah lain/i)).toBeInTheDocument()
+        const { getByText } = render(<CustomOptionCard />);
+        expect(getByText(/Jumlah Lot/i)).toBeInTheDocument();
     })
 
-    it('should be checked when clicked', () => {
+    it('should show the right amount', () => {
         const { getByTestId } = render(<CustomOptionCard />)
+        const textBox = getByTestId("mi-custom-amount");
         const radioButton = getByTestId("mi-radio-btn");
+        userEvent.click(radioButton);
 
-        userEvent.click(radioButton)
+        textBox.value='';
+        userEvent.type(textBox, 100);
         expect(radioButton).toBeChecked();
+        expect(textBox.value).toHaveValue(100);
+
+        textBox.value='';
+        userEvent.type(textBox, -9);
+        expect(textBox.value).toHaveValue(1);
+
+        textBox.value='';
+        userEvent.type(textBox, 1000);
+        expect(textBox.value).toHaveValue(100);
+
+        textBox.value='';
+        userEvent.type(textBox, 'a');
+        expect(textBox.value).toHaveValue('');
     })
 })
